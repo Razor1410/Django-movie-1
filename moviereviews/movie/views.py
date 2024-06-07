d7e5a7d3362106b90eea7d8a7d9abd60b71824a1
razor1410 <razor-apps@protonmail.com>
2024-06-07 09:57:37 +0200

    07.06 9:57

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('<h2>Witam na stronie głównej</h2>')
+
+
+def about(request):
+    return HttpResponse('<h1>Strona o mnie</h1>')
